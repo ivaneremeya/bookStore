7cea79fd8d6d21631b4a76e1197b356f3c846724
ivaneremeya <127229725+ivaneremeya@users.noreply.github.com>
2024-04-09 12:50:59 +0300

    Delete my-app directory

--- my-app/src/entities/cartItem/api/getBookStock.ts
+++
@@ -1,6 +0,0 @@
-import { AxiosInstance } from "../../../shared/api/auth";
-
-export const GetStockBook = async (id: number) => {
-  const response = await AxiosInstance.get(`/api/books/${id}/`);
-  return response.data;
-};
--- my-app/src/shared/ui/Loader/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import CircularProgress from "@mui/material/CircularProgress";
-import styles from "./loader.module.scss";
-
-export const Loader = () => (
-  <div className={styles.loader}>
-    <CircularProgress />
-  </div>
-);
